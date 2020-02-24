a6227ecbad0c6201ad15105a44a820623af61553
Jamie Tartell <jetartell@gmail.com>
2020-02-24 23:08:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   delete newObj.key
+  return newObj
+}
+
 
 function updateObjectWithKeyAndValue(obj, key, value) {
  
